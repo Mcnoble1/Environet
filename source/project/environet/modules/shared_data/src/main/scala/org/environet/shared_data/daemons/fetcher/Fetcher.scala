16abf70f57ec9bd3aee093a004d1e5e1d020381f
Idowu Festus <idowufestustemiloluwa@gmail.com>
2024-09-11 21:43:22 +0100

    Delete source/project/environet/modules/shared_data/src/main/scala/org/environet/shared_data/daemons directory

--- source/project/environet/modules/shared_data/src/main/scala/org/environet/shared_data/daemons/fetcher/Fetcher.scala
+++
@@ -1,9 +0,0 @@
-package org.elpaca_metagraph.shared_data.daemons.fetcher
-
-import org.elpaca_metagraph.shared_data.types.DataUpdates.ElpacaUpdate
-
-import java.time.LocalDateTime
-
-trait Fetcher[F[_]] {
-  def getAddressesAndBuildUpdates(currentDate: LocalDateTime): F[List[ElpacaUpdate]]
-}
